2151dba1615c838d63ac6cff70adb65a42c87982
NogueiraLorena <118185045+NogueiraLorena@users.noreply.github.com>
2023-01-23 16:11:15 -0300

    Add files via upload

---
+++ src/controller/OlaMundo.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+
+System.out.println("Hello World. ");
+	
+
+	
+	
+	
+	}
+
+}
---
+++ src/controller/TestandoOInfinito.java
@@ -0,0 +1,36 @@
+package controller;
+
+public class TestandoOInfinito {
+
+	public static void main(String[] args) {
+
+		
+        double posInf = Double.POSITIVE_INFINITY;
+        double negInf = Double.NEGATIVE_INFINITY;
+        System.out.println(posInf);
+        System.out.println(negInf);
+
+        
+   //A execução disso foi decepcionante.
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
